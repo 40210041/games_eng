6f125a0f2f658b4d252ec947330851b686691b33
40210041 <40210041@live.napier.ac.uk>
2018-01-16 15:01:43 +0000

    added practical_2 folder, updated cmakelist.txt

---
+++ practical_2/main.cpp
@@ -0,0 +1,33 @@
+//main.cpp
+//space invaders
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+//load
+void Load()
+{
+
+}
+
+//update
+void Update()
+{
+
+}
+
+
+//Render
+void Render()
+{
+
+}
+
+
+//main
+void main()
+{
+
+}
