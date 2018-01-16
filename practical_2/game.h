1491c68d714920668a7058c4cd4e1c79e2b00c84
40210041 <40210041@live.napier.ac.uk>
2018-01-16 16:51:14 +0000

    created .cpp and .h

---
+++ practical_2/game.h
@@ -0,0 +1,11 @@
+//game.h
+
+#pragma once
+#include <SFML/Graphics.hpp>
+
+constexpr unit16_t game_width = 800;
+constexpr unit16_t game_height = 600;
+constexpr unit16_t invaders_rows = 5;
+constexpr unit16_t invaders_columns = 12;
+
+extern sf::Texture spritesheet;
--- practical_2/main.cpp
+++ practical_2/main.cpp
@@ -2,14 +2,25 @@
 //space invaders
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "ship.h"
+
+std::vector<Ship *>ships;
 
 using namespace sf;
 using namespace std;
 
+sf::Texture spritesheet;
+sf::Sprite invader;
+
 //load
 void Load()
 {
-
+  // if (!spritesheet.loadFromFile("res/img/invaders_sheet.png"))
+  // {
+  //   cerr << "Failed to load spritesheet!" << std::endl;
+  // }
+  // invader.setTexture(spritesheet);
+  // sprite.setTextureRect(sf::IntRect(0,0,32,32));
 }
 
 //update
@@ -22,7 +33,7 @@ void Update()
 //Render
 void Render()
 {
-
+  // window.draw(invader);
 }
 
 
