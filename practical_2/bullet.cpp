9a722f3a2e816921495beb9cf217e6e9f814778e
40210041 <40210041@live.napier.ac.uk>
2018-01-22 22:38:49 +0000

    up to 3.7.1 of work book

---
+++ practical_2/bullet.cpp
@@ -0,0 +1,14 @@
+//bullet.cpp
+#include "bullet.h"
+#include "game.h"
+
+using namespace sf;
+using namespace std;
+
+
+Bullet::Bullet() : Sprite() {}; //check for answer
+
+void Bullet::Update(const float &dt)
+{
+  move(0, dt * 200.0f * (_mode ? 1.0f : -1.0f));
+}
--- practical_2/bullet.h
+++ practical_2/bullet.h
@@ -2,4 +2,15 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class Bullet :
+class Bullet : public sf::Sprite
+{
+public:
+  void Update(const float &dt);
+  Bullet(const sf::Vector2f &pos, const bool mode);
+  ~Bullet()=default;
+
+protected:
+  Bullet();
+  //false = player bullet, true = enemy bullet
+  bool _mode;
+};
