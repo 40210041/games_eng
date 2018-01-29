c783987f1d4a5bcf856955ee687ae45bee04f870
40210041 <40210041@live.napier.ac.uk>
2018-01-29 19:45:09 +0000

    fixing up branches

--- practical_2/ship.h
+++ practical_2/ship.h
@@ -1,4 +1,5 @@
 //ship.h
+
 #pragma once
 #include <SFML/Graphics.hpp>
 
@@ -6,37 +7,26 @@ class Ship : public sf::Sprite
 {
 protected:
   sf::IntRect _sprite;
-
   //default constructor is hidden
   Ship();
+
 public:
   //constructor that takes a sprite
   explicit Ship(sf::IntRect ir);
-
   //pure virtual deconstructor -- makes this an abstract class
   virtual ~Ship() = 0;
-
   //update, virtual so can be overrided, but not pure virtual
   virtual void Update(const float &dt);
 };
 
-
+//3.5
 class Invader : public Ship
 {
 public:
-  static bool direction; //can be accessed anywhere
-  static float speed; // ""
+  //3.5.1
+  static bool direction;
+  static float speed;
   Invader(sf::IntRect ir, sf::Vector2f pos);
   Invader();
-  void Update(const float &dt) override;
-};
-
-
-class Player : public Ship
-{
-public:
-  //static bool p_direction;
-  //static float p_speed;
-  Player();
   void Update(const float &dt) override;
 };
