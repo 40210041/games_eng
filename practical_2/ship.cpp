d78b752b8a7e600163ccb95582d2c4996ffcf0db
40210041 <40210041@live.napier.ac.uk>
2018-01-29 19:44:49 +0000

    fixing up branches

--- practical_2/ship.cpp
+++ practical_2/ship.cpp
@@ -1,18 +1,12 @@
 //ship.cpp
+
 #include "ship.h"
-#include "game.h"
-//#include "bullet.h"
+#include "game.h" //for spritesheet
+
 using namespace sf;
 using namespace std;
-
 bool Invader::direction;
 float Invader::speed;
-
-const Keyboard::Key controls[3] = {
-  Keyboard::Left, //left key
-  Keyboard::Right, //right key
-  Keyboard::Space, //shoot
-};
 
 Ship::Ship() {};
 
@@ -34,17 +28,18 @@ Invader::Invader() : Ship() {}
 
 Invader::Invader(sf::IntRect ir, sf::Vector2f pos) : Ship(ir)
 {
-  setOrigin(16,16);
+  setOrigin(16, 16);
   setPosition(pos);
 }
 
-
 void Invader::Update(const float &dt)
 {
-  //call base ship::update() to run generic ship logic
+  //call base ship::update to run generic ship logic
   Ship::Update(dt);
+
+  //3.5.1
   //move left/right dictated by speed var
-  move(dt * (direction ? 1.0f : -1.0f) * speed, 0);
+  move(dt* (direction ? 1.0f : -1.0f) * speed, 0);
 
   //detect whether to drop or reverse
   if ((direction && getPosition().x > game_width - 16) ||
@@ -57,40 +52,3 @@ void Invader::Update(const float &dt)
     }
   }
 }
-
-
-// Player::Player() : Ship(IntRect(160,32,32,32))
-// {
-//   setPosition({game_height * .5f, game_height - 32.f});
-// }
-//
-//
-// // STILL NEED TO DO THIS!!! //
-// void Player::Update(const float &dt)
-// {
-//   float move_player = 0.0f;
-//   float move_speed = 400.f;
-//
-//   Ship::Update(dt);
-//
-//   if (Keyboard::isKeyPressed(controls[0])){   //move Left
-//     move_player--;
-//   }
-//   if (Keyboard::isKeyPressed(controls[1])){   //move right
-//     move_player++;
-//   }
-//   //move(move_player * move_speed * dt, 0);
-//   player.move(move_player * move_speed * dt, 0); //check for answer
-
-  // //its bullet time
-  // static vector<Bullet* > bullets;
-  // if (Keyboard::isKeyPressed(controls[3]))
-  // {
-  //   bullets.push_back(new Bullet(getPosition(), false));
-  // }
-  // for (const auto s : bullets)
-  // {
-  //   bullets.Update(dt);
-  // }
-
-// }
